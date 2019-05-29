19478ff69e7e8ad9ed8bfb73e8d519ede832ebe9
Blairgendary <blair.w.matthew@gmail.com>
2019-05-29 01:35:41 -0600

    Google Maps + 360 Youtube API

--- platforms/browser/www/js/script.js
+++ platforms/browser/www/js/script.js
@@ -1,21 +1,20 @@
-var targetLat = 49.695939;
-var targetLong = -112.896976;
+var targetLat = 49.695764;
+var targetLong = -112.895013;
 var deviceLat;
 var deviceLong;
 var videoVisible = 0;
 var distanceFrom = 0.00014;
-var dWidth = window.innerWidth;
-var dHeight = window.innerHeight;
 
 var geoOpt = { 
-    maximumAge: 3000,
+    maximumAge: 1000,
     timeout: 5000,
     enableHighAccuracy: true
 }
+
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() { 
-    navigator.geolocation.watchPosition(onSuccess, onError, geoOpt);
+    navigator.geolocation.getCurrentPosition(onSuccess, onError, geoOpt);
 }
 
 function onSuccess(postion) { 
@@ -27,12 +26,18 @@ function onSuccess(postion) {
                         'Accuracy: '           + postion.coords.accuracy    + '<br / >' + 
                         'Target Latitude: '    + targetLat                  + '<br / >' +
                         'Target Longitude: '   + targetLong                 + '<br / >';
+    
                         deviceLat = postion.coords.latitude;
                         deviceLong = postion.coords.longitude;
+                        //targetLat = deviceLat;
+                        //targetLong = deviceLong;
+                        getMap(deviceLat,deviceLong);
+    
+    
                         if (((deviceLat < targetLat + distanceFrom) && (deviceLat > targetLat - distanceFrom)) && ((deviceLong > targetLong - distanceFrom) && (deviceLong < targetLong + distanceFrom))) { 
                             element.innerHTML = "";
                             if (videoVisible == false) { 
-                                video.innerHTML = '<iframe width="1920" height="1080" src="https://www.youtube.com/embed/tgbNymZ7vqY?"></iframe>';
+                                document.location.href = 'pages/player.html';
                                 //element.innerHTML = 'You have found the TARGET'; 
                                 videoVisible = true;
                             }
@@ -41,10 +46,66 @@ function onSuccess(postion) {
                         video.innerHTML = "";
                         }
 }   
+
+function getMap(deviceLat,deviceLong) { 
     
+    var mapOptions = {
+        center: new google.maps.LatLng(0, 0),
+        zoom: 1,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+
+    var latLong = new google.maps.LatLng(deviceLat, deviceLong);
+    var tlatLong = new google.maps.LatLng(targetLat, targetLong);
+
+    var marker = new google.maps.Marker({
+        position: latLong
+    });
+    
+    var targetMarker = new google.maps.Marker({
+    position: tlatLong,
+    title:"Reach this Point"
+    });
+
+    marker.setMap(map);
+    targetMarker.setMap(map);
+    map.setZoom(16.5);
+    map.setCenter(marker.getPosition());
+    
+}
+
+var onMapWatchSuccess = function (position) {
+
+    var updatedLatitude = position.coords.latitude;
+    var updatedLongitude = position.coords.longitude;
+
+    if (updatedLatitude != deviceLat && updatedLongitude != deviceLong) {
+
+        deviceLat = updatedLatitude;
+        deviceLong= updatedLongitude;
+
+        getMap(updatedLatitude, updatedLongitude);
+    }
+}
+
+// Error Checking
 function onError(error) { 
-    aler('code: ' + error.code + '\n' + 
+    alert('code: ' + error.code + '\n' + 
          'message: ' + error.message + '\n');
 }
+
+function onMapError(error) {
+    console.log('code: ' + error.code + '\n' +
+        'message: ' + error.message + '\n');
+}
+
+function watchMapPosition() {
+
+    return navigator.geolocation.watchPosition(onMapWatchSuccess, onMapError, { enableHighAccuracy: true });
+}
+
 
 
---
+++ www/js/player.js
@@ -0,0 +1,31 @@
+    let player;
+    let panStarted = false;
+
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+            videoId: 'sQwMI4hkIPs',
+            events: {
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+
+    // Start animation when video starts playing.
+    function onPlayerStateChange(event) {
+        if (event.data == 1 && !panStarted) {
+            requestAnimationFrame(panVideo);
+            panStarted = true;
+        }
+    }
+
+    function panVideo() {
+        // 20 seconds per rotation.
+        const yaw = (performance.now() / 1000 / 20 * 360) % 360;
+        // 2 up-down cycle per rotation.
+        const pitch = 20 * Math.sin(2 * yaw / 360 * 2 * Math.PI);
+        player.setSphericalProperties({
+            yaw: yaw,
+            pitch: pitch
+        });
+        requestAnimationFrame(panVideo);
+    }
