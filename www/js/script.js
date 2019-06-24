407325a817ec8a19c06de724ffb9e15d0b6a5907
Blairgendary <blair.w.matthew@gmail.com>
2019-06-24 09:35:47 -0600

    This build has reactive functionality with google forms and spreadsheets

--- www/js/script.js
+++ www/js/script.js
@@ -1,7 +1,66 @@
 var deviceLat;
 var deviceLong;
+var R = Math.pow(6367,3);
 var distanceFrom = 0.0003;
+var latLong;
 var marker = 0;
+var targets = [];
+var targetsMarkers = [];
+var videoReady = false;
+var latUrl = "https://sheets.googleapis.com/v4/spreadsheets/1KhDmsypOJHUxTTQ7cmeX9H8IuLs7eccFxa2HYuG8wBo/values/Loc!B2:B?key=APIKEYHERE";
+var lonUrl = "https://sheets.googleapis.com/v4/spreadsheets/1KhDmsypOJHUxTTQ7cmeX9H8IuLs7eccFxa2HYuG8wBo/values/Loc!C2:C?key=APIKEYHERE";
+var vidUrl = "https://sheets.googleapis.com/v4/spreadsheets/1KhDmsypOJHUxTTQ7cmeX9H8IuLs7eccFxa2HYuG8wBo/values/Loc!D2:D?key=APIKEYHERE";
+
+var latData;
+var lonData;
+var vidData;
+
+var latitudes = [];
+var longitudes = [];
+var videoUrls = [];
+
+// ----------------------------------RETRIEVE ALL LATITUDES---------------------------------------- 
+var xhr = new XMLHttpRequest();
+var xhr2 = new XMLHttpRequest();
+var xhr3 = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() { 
+    if (this.readyState == 4 & this.status == 200) {
+        latData = jQuery.parseJSON(xhr.responseText);
+        for(var i = 0; i < latData.values.length; i++) {
+        latitudes[i] = parseFloat(latData.values[i]);
+        console.log(latitudes[i]);
+        }  
+    }
+};
+xhr.open('GET', latUrl);
+xhr.send();
+
+// ----------------------------------RETRIEVE ALL LONGITUDES----------------------------------------  
+xhr2.onreadystatechange = function() { 
+    if (this.readyState == 4 & this.status == 200) {
+        lonData = jQuery.parseJSON(xhr2.responseText);
+        for(var i = 0; i < lonData.values.length; i++) {
+        longitudes[i] = parseFloat(lonData.values[i]);
+        console.log(longitudes[i]);
+        }
+    }
+};
+xhr2.open('GET', lonUrl);
+xhr2.send();
+
+// ----------------------------------RETRIEVE ALL VIDEO LINKS----------------------------------------
+xhr3.onreadystatechange = function() { 
+    if (this.readyState == 4 & this.status == 200) {
+        vidData = jQuery.parseJSON(xhr3.responseText);
+        for(var i = 0; i < vidData.values.length; i++) {
+        videoUrls[i] = vidData.values[i];
+        console.log(videoUrls[i]);
+        }
+    }
+};
+xhr3.open('GET', vidUrl);
+xhr3.send();
 
 // ----------------------------------All target locations----------------------------------------
 var research = {latitude:49.67771, longitude:-112.85961};
@@ -15,6 +74,16 @@ var geoOpt = {
 }
 
 document.addEventListener("deviceready", onDeviceReady, false);
+
+// Converts from degrees to radians.
+Math.radians = function(degrees) {
+  return degrees * Math.PI / 180;
+};
+ 
+// Converts from radians to degrees.
+Math.degrees = function(radians) {
+  return radians * 180 / Math.PI;
+};
 
 function onDeviceReady() { 
     navigator.geolocation.getCurrentPosition(onSuccess, onError, geoOpt);
@@ -32,6 +101,8 @@ function onSuccess(position) {
     
                         deviceLat = position.coords.latitude;
                         deviceLong = position.coords.longitude;
+                        //research.latitude = deviceLat;
+                        //research.longitude = deviceLong;
                         getMap(deviceLat,deviceLong);
                         
 }  
@@ -47,30 +118,27 @@ function getMap(deviceLat,deviceLong) {
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
 
-    var latLong = new google.maps.LatLng(deviceLat, deviceLong);
-    var target1 = new google.maps.LatLng(research.latitude, research.longitude);
-    var target2 = new google.maps.LatLng(uhall.latitude, uhall.longitude);
-
+    latLong = new google.maps.LatLng(deviceLat, deviceLong);
+    
     marker = new google.maps.Marker({
-        position: latLong
+        position: latLong,
+        map: map
     });
     
-    var target1Marker = new google.maps.Marker({
-    position: target1,
-    title:"Reach this Point"
-    });
+    for(var i = 0; i < latitudes.length; i++) { 
+    targets[i] = new google.maps.LatLng(latitudes[i],longitudes[i]);
+    }
     
-    var target2Marker = new google.maps.Marker({
-    position: target2,
-    title:"Reach this Point"
-    });
+    for(var i = 0; i < latitudes.length; i++) { 
+        targetsMarkers[i] = new google.maps.Marker({
+        position: targets[i],
+        map: map
+        });
+    }
 
     marker.setMap(map);
-    target1Marker.setMap(map);
-    target2Marker.setMap(map);
     map.setZoom(16.5);
     map.setCenter(marker.getPosition());
-    
 }
 
 var onMapWatchSuccess = function (position) {
@@ -93,26 +161,12 @@ var onMapWatchSuccess = function (position) {
     var latLong = new google.maps.LatLng(updatedLatitude, updatedLongitude);
     marker.setPosition(latLong);
     
-    t1UpLat = research.latitude + distanceFrom;
-    t1LoLat = research.latitude - distanceFrom;
-    t1UpLng = research.longitude + distanceFrom;
-    t1LoLng = research.longitude - distanceFrom;
-    
-    t2UpLat = uhall.latitude + distanceFrom;
-    t2LoLat = uhall.latitude - distanceFrom;
-    t2UpLng = uhall.longitude + distanceFrom;
-    t2LoLng = uhall.longitude - distanceFrom;
-    
-    if (deviceLat <= t1UpLat && deviceLat >= t1LoLat && deviceLong <= t1UpLng && deviceLong >= t1LoLng) {
-    localStorage.clear();
-    localStorage.setItem("videoID",'fnHQZJcBy9Q');
-    location.href = 'pages/player.html';
-    }
-    if (deviceLat <= t2UpLat && deviceLat >= t2LoLat && deviceLong <= t2UpLng && deviceLong >= t2LoLng) {
-    localStorage.clear();
-    localStorage.setItem("videoID",'sQwMI4hkIPs');
-    location.href = 'pages/player.html';
-    }
+    var dlat = Math.radians(latitudes[0] - deviceLat);
+    var dlon = Math.radians(longitudes[0] - deviceLong);
+    var a = Math.sin(dlat/2) * Math.sin(dlat/2) + Math.radians(Math.cos(deviceLat)) * Math.radians(Math.cos(latitudes[0])) * Math.sin(dlon/2) * Math.sin(dlon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    console.log(d);
 }
 
 // Error Checking
